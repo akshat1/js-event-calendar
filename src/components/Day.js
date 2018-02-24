c6749535d2922968cf84b223053b643a5fc2f6d1
akshat1 <akshat.sh@gmail.com>
2018-02-23 23:15:28 -0800

    More tweaking of borders

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,23 +1,27 @@
 const hyperHTML = require('hyperhtml/cjs').default;
 const { EventMarker } = require('./EventMarker');
 const { leftPad, classNames } = require('../utils');
-const { isDayFiller } = require('../utils/date');
+const { isDayFiller, getLastDayOfMonth } = require('../utils/date');
 
 function Day(props = {}) {
   const {
     dayOfMonth,
-    events = []
+    dayOfWeek,
+    events = [],
+    month,
+    year
   } = props;
 
   const className = classNames(
     'simian-calendar__day', {
       'simian-calendar__day--filler': isDayFiller(props),
-      'simian-calendar__day--first-day': dayOfMonth === 0
+      'simian-calendar__day--first-day': dayOfMonth === 0,
+      'simian-calendar__day--last-day': dayOfMonth + 1 === getLastDayOfMonth(month, year)
     }
   );
 
   return hyperHTML.wire(props)`
-    <div class=${className} data-day=${dayOfMonth}>
+    <div class=${className} data-dom=${dayOfMonth} data-dow=${dayOfWeek}>
       <div class="simian-calendar__day-number">${leftPad(dayOfMonth + 1, '0', 2)}</div>
       <div class="simian-calendar__event-markers">
         ${events.map(e => new EventMarker({...e}))}
